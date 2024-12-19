df81afa67128291efe25b068c79c41647f35edfa
SSR707 <samandarshavkatov07@gmail.com>
2024-12-19 17:33:51 +0500

    HomeWork

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/http-exception/http-excaption';
-
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter());
+  const config = new DocumentBuilder()
+    .setTitle('cleaning app')
+    .setDescription('The cleaning api')
+    .setVersion('1.0')
+    .addTag('cleaning')
+    .build();
+  const documentFactory= () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -10,23 +10,29 @@ import {
   IsStrongPassword,
 } from 'class-validator';
 import { Role } from 'src/interface/interface';
+import { ApiProperty } from '@nestjs/swagger';
 export class CreateUserDto {
+  @ApiProperty({name: 'first_name', example:'Jon' })
   @IsString()
   @IsNotEmpty()
   first_name: string;
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({name: 'last_name', example:'Doe' })
   last_name: string;
   @IsNumber()
   @IsNotEmpty()
+  @ApiProperty({name: 'age', example:20 })
   age: number;
   @IsString()
   @IsEmail()
   @IsNotEmpty()
+  @ApiProperty({name: 'email', example:'jondoe@gmail.com' })
   email: string;
   @IsString()
   @IsStrongPassword()
   @IsNotEmpty()
+  @ApiProperty({name: 'password', example:'jon004' })
   password: string;
 
   @IsString()
