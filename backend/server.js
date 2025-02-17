048bbeb6514b2690916503067aa93924667fb2c2
SSR707 <samandarshavkatov07@gmail.com>
2025-02-17 16:03:57 +0500

    Home Work

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,6 @@ import { userRoutes } from "./src/routes/users.routes.js";
 import cors from "cors";
 
 const app = express();
-app.use(
-  cors({
-    origin: "http://127.0.0.1:5500",
-  })
-);
 app.use(express.json());
 
 app.use("/users", userRoutes);
