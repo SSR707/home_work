2fbf498518035820147211e2f2fb5e16a0efe82c
SSR707 <samandarshavkatov07@gmail.com>
2025-02-18 08:53:58 +0500

    home work

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,10 @@ import cors from "cors";
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "https://home-work-besic-todo-task.netlify.app",
+      "http://localhost:5173",
+    ],
   })
 );
 app.use(express.json());
