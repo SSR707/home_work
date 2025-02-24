31eaef3f5d18d33eda3742123e1adfbed33fffd2
SSR707 <samandarshavkatov07@gmail.com>
2025-02-24 16:50:35 +0500

    Home Work end

--- backend/src/model/users.model.js
+++ backend/src/model/users.model.js
@@ -1,8 +1,36 @@
 import mongoose from "mongoose";
 
 const userSchema = mongoose.Schema({
-  title: {
+  id: {
     type: String,
+    required: true,
+    unique: true,
+  },
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 3,
+    maxlength: 50,
+  },
+  age: {
+    type: Number,
+    required: true,
+    min: 0,
+    max: 120,
+  },
+  address: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    match: [/^\S+@\S+\.\S+$/, "Please enter a valid email address"],
   },
 });
 
