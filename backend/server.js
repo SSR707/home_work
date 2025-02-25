7836107b65bf3b0c7a99687761db2dacf0d13b7b
SSR707 <samandarshavkatov07@gmail.com>
2025-02-25 18:11:38 +0500

    Home Work End

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,12 @@ const app = express();
 app.use(
   cors({
     origin: "*",
+    methods: "GET,POST,PUT,DELETE,OPTIONS",
+    allowedHeaders: "*",
   })
 );
+app.options("*", cors());
+
 app.use(express.json());
 
 app.use("/users", userRoutes);
